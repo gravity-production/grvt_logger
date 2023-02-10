06a95582d99bb07641c06970aa9beb9f1963ac4b
Alexandr <efimchiksasha@gmail.com>
2023-02-10 13:37:00 +0300

    initial commit

---
+++ logger.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Alexandr on 2/7/2023.
+//
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+
+#include "logger.hpp"
+
+std::array<std::string, sizeof(logger::log_level)+1u> logger::level_string = {
+		"TRACE", "DEBUG", "INFO", "WARNING", "ERROR"
+};
+
+void logger::log(logger::log_level level, const char* source, const std::string& message)
+{
+	std::stringstream formatted;
+	formatted << "[" << level_string[level] << "]";
+	std::time_t tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::tm tm = *std::gmtime(&tt); //GMT (UTC)
+	formatted << " - " << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+#ifdef DEBUG
+	formatted << " - " << source;
+#endif
+	formatted << " - " << message;
+	std::cout << formatted.str() << std::endl;
+}
+void logger::log(const char* source, const std::string& message)
+{
+	log(log_level::DEBUG_LEVEL, source, message);
+}
+void logger::simple_out(const std::string& message)
+{
+	std::cout << message << std::endl;
+}
---
+++ logger.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Alexandr on 2/7/2023.
+//
+
+#ifndef GMA2PATCH_LOGGER_HPP
+#define GMA2PATCH_LOGGER_HPP
+
+#include <iostream>
+#include <array>
+
+class logger {
+
+public:
+	enum log_level {
+		TRACE_LEVEL,
+		DEBUG_LEVEL,
+		INFO_LEVEL,
+		WARNING_LEVEL,
+		ERROR_LEVEL
+	};
+	static void log(log_level, const char* source, const std::string& message);
+	static void log(const char* source, const std::string& message);
+	static void simple_out(const std::string& message);
+private:
+	static std::array<std::string, sizeof(log_level)+1u> level_string;
+};
+
+
+#define STR_(x) #x
+#define STR(x) STR_(x)
+#define LOG_SIMPLE_OUT(message) logger::simple_out(message)
+#define LOG_TRACE(message) logger::log(logger::TRACE_LEVEL, __FILE__ ":" STR(__LINE__), message)
+#define LOG_INFO(message) logger::log(logger::INFO_LEVEL, __FILE_NAME__ ":" STR(__LINE__), message)
+#define LOG_WARNING(message) logger::log(logger::WARNING_LEVEL, __FILE_NAME__ ":" STR(__LINE__), message)
+#define LOG_ERROR(message) logger::log(logger::ERROR_LEVEL, __FILE_NAME__ ":" STR(__LINE__), message)
+
+#endif //GMA2PATCH_LOGGER_HPP
